5c3fd78f1914cf919a4c62454b9036637c251b3f
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-02-27 12:52:24 -0300

    feat: Import choose_an_alternative function

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import telebot
 import os
 from dotenv import load_dotenv
-from AI_API_Handler.ai_handler import make_a_question
+from AI_API_Handler.ai_handler import make_a_question, choose_an_alternative
 
 load_dotenv()
 
