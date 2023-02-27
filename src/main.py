5ea12f1c7774758922d70dac71f4fc2bd6bc23dc
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-02-27 11:33:53 -0300

    feat: Load environment variables

--- src/main.py
+++ src/main.py
@@ -2,3 +2,8 @@ import telebot
 import os
 from dotenv import load_dotenv
 from AI_API_Handler.ai_handler import make_a_question
+
+load_dotenv()
+
+TELEGRAM_API_KEY = os.getenv("TELEGRAM_KEY")
+bot = telebot.TeleBot(str(TELEGRAM_API_KEY))
