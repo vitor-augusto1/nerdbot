ab775904cc9fae4def95baf43c143b7d2147813d
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-02-27 11:34:32 -0300

    feat: Add new bot message handler for `start` user message

--- src/main.py
+++ src/main.py
@@ -7,3 +7,9 @@ load_dotenv()
 
 TELEGRAM_API_KEY = os.getenv("TELEGRAM_KEY")
 bot = telebot.TeleBot(str(TELEGRAM_API_KEY))
+
+@bot.message_handler(commands=['start'])
+def start_message(message) -> None:
+    CHAT_ID = message.chat.id
+    bot.send_message(CHAT_ID, "Hello, world!")
+
