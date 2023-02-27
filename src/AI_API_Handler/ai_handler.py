0bee3f698d50754f1eb59d277a849679addf8e11
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-02-27 16:41:45 -0300

    feat: Add rate limit exception

--- src/AI_API_Handler/ai_handler.py
+++ src/AI_API_Handler/ai_handler.py
@@ -8,32 +8,52 @@ load_dotenv()
 openai.api_key = os.getenv('TELEGRAM_API_KEY')
 
 def make_a_question(user_question: str) -> str:
-    user_question_formated = user_question.strip()
-    response = openai.Completion.create(
-      model="text-davinci-003",
-      prompt=f"Q: {user_question_formated}?\nA:",
-      temperature=0.7,
-      max_tokens=3000,
-      top_p=1,
-      best_of=2,
-      frequency_penalty=0.7,
-      presence_penalty=0,
-      stop=["\n"]
-    )
-    json_object_response = json.loads(str(response))
-    AI_answer = json_object_response["choices"][0]["text"]
-    return AI_answer
+    try:
+        user_question_formated = user_question.strip()
+        response = openai.Completion.create(
+          model="text-davinci-003",
+          prompt=f"Q: {user_question_formated}?\nA:",
+          temperature=0.7,
+          max_tokens=3000,
+          top_p=1,
+          best_of=2,
+          frequency_penalty=0.7,
+          presence_penalty=0,
+          stop=["\n"]
+        )
+        print(response)
+        json_object_response = json.loads(str(response))
+        AI_answer = json_object_response["choices"][0]["text"]
+        empty_string = ""
+        if AI_answer == empty_string or AI_answer == " ":
+            error_message = "Não foi possivel gerar a sua resposta. Tente novamente"
+            return error_message
+        return AI_answer
+    except openai.error.RateLimitError as e:
+        return "Erro ao gerar resposta. Tente novamente."
+
+
 
 def choose_an_alternative(alternatives: str) -> str:
-    response = openai.Completion.create(
-      model="text-davinci-003",
-      prompt=alternatives.strip(),
-      temperature=0.2,
-      max_tokens=3000,
-      top_p=1,
-      frequency_penalty=0.2,
-      presence_penalty=0
-    )
-    json_object_response = json.loads(str(response))
-    AI_answer = json_object_response["choices"][0]["text"]
-    return AI_answer
+    try:
+        response = openai.Completion.create(
+          model="text-davinci-003",
+          prompt=alternatives.strip(),
+          temperature=0.2,
+          max_tokens=3000,
+          top_p=1,
+          frequency_penalty=0.2,
+          presence_penalty=0
+        )
+        print(response)
+        json_object_response = json.loads(str(response))
+        AI_answer = json_object_response["choices"][0]["text"]
+        empty_string = ""
+        if AI_answer == empty_string or AI_answer == " ":
+            error_message = (
+                f"Não foi possivel escolher uma alternativa. Tente novamente!"
+            )
+            return error_message
+        return AI_answer
+    except openai.error.RateLimitError as e:
+        return "Erro ao gerar resposta. Tente novamente."
