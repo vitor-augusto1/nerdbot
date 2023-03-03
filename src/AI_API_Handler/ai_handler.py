dbeeaea1e24a69a85065e4eb3c1e286d9cdcf401
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-03-03 11:50:26 -0300

    feat: Load dotenv

--- src/AI_API_Handler/ai_handler.py
+++ src/AI_API_Handler/ai_handler.py
@@ -1,9 +1,9 @@
 import json
 import openai
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 import os
 
-#load_dotenv()
+load_dotenv()
 
 openai.api_key = os.getenv('TELEGRAM_API_KEY')
 
