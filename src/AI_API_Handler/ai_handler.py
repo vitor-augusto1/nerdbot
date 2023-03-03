9991f4bbb8c4f17c50c077199f35ddbb48369fff
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-03-03 15:29:54 -0300

    feat: Add new `summarize_text` function

--- src/AI_API_Handler/ai_handler.py
+++ src/AI_API_Handler/ai_handler.py
@@ -6,6 +6,7 @@ import os
 load_dotenv()
 
 openai.api_key = os.getenv('TELEGRAM_API_KEY')
+
 def choose_an_alternative(alternatives: str) -> str:
     try:
         response = openai.Completion.create(
@@ -18,6 +19,30 @@ def choose_an_alternative(alternatives: str) -> str:
           presence_penalty=0
         )
         print(response)
+        json_object_response = json.loads(str(response))
+        AI_answer = json_object_response["choices"][0]["text"]
+        empty_string = ""
+        if AI_answer == empty_string or AI_answer == " ":
+            error_message = (
+                f"Não foi possivel escolher uma alternativa. Tente novamente!"
+            )
+            return error_message
+        return AI_answer
+    except openai.error.RateLimitError as e:
+        return "Erro ao gerar resposta. Tente novamente."
+
+
+def summarize_text(text: str) -> str:
+    try:
+        response = openai.Completion.create(
+          model="text-davinci-003",
+          prompt=f"{text}.\n\nTl;dr",
+          temperature=0.7,
+          max_tokens=60,
+          top_p=1.0,
+          frequency_penalty=0.0,
+          presence_penalty=1
+        )
         json_object_response = json.loads(str(response))
         AI_answer = json_object_response["choices"][0]["text"]
         empty_string = ""
